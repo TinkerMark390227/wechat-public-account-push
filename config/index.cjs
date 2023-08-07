a3c0fa799a737af3b7ecc9a7b9f29ad035befb50
TinkerMark390227 <1197509212@qq.com>
2023-08-07 10:28:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oixnN6aVZEIZNtq7uKFVaJOCAIiM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vpt5NfBuHmY_PkoSJTRpjACnA2qgE4T8Cy7KUkek5GY',
+      useTemplateId: 'LXT2g1Kruh9sMiZeFz7I3l8T5hwtWvnSuENExzRs1yk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
